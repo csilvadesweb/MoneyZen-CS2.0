95d63b686a4c6acaa4949c1e062b9f7a6817827a
C.Silva <csdesweb@gmail.com>
2026-01-08 16:49:00 -0300

    Atualizar o service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,39 +1,16 @@
 const CACHE = "moneyzen-v1";
-
 const FILES = [
-  "/MoneyZen-CS2.0/",
-  "/MoneyZen-CS2.0/index.html",
-  "/MoneyZen-CS2.0/style.css",
-  "/MoneyZen-CS2.0/script.js",
-  "/MoneyZen-CS2.0/manifest.json",
-  "/MoneyZen-CS2.0/privacy.html",
-  "/MoneyZen-CS2.0/icons/icon-72.png",
-  "/MoneyZen-CS2.0/icons/icon-96.png",
-  "/MoneyZen-CS2.0/icons/icon-128.png",
-  "/MoneyZen-CS2.0/icons/icon-144.png",
-  "/MoneyZen-CS2.0/icons/icon-152.png",
-  "/MoneyZen-CS2.0/icons/icon-172.png",
-  "/MoneyZen-CS2.0/icons/icon-192.png",
-  "/MoneyZen-CS2.0/icons/icon-384.png",
-  "/MoneyZen-CS2.0/icons/icon-512.png"
+  "./",
+  "./index.html",
+  "./style.css",
+  "./script.js",
+  "./manifest.json"
 ];
 
 self.addEventListener("install", e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)));
-  self.skipWaiting();
-});
-
-self.addEventListener("activate", e => {
-  e.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(keys.map(k => k !== CACHE && caches.delete(k)))
-    )
-  );
-  self.clients.claim();
 });
 
 self.addEventListener("fetch", e => {
-  e.respondWith(
-    caches.match(e.request).then(r => r || fetch(e.request))
-  );
+  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
 });
